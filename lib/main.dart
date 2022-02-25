d32280586b9d99b07c7710b4c6de5ae53295d000
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-02-26 00:21:32 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    // MaterialApp app=MaterialApp()
+    // return app()
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  ElevatedButton(
+            onPressed: (){
+              print("Button Has been clicked");}
+            , child: const Text('Button')),
+      ),
+
+    );
+  }
+}
+
+
+
+
+
